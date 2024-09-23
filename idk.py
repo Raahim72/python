5d37bd3e5fef9d76cfb40d3fc4705f43aadf2327
Raahim72 <raahimhussain80@gmail.com>
2024-09-23 18:00:59 +0500

    absolute value

---
+++ idk.py
@@ -0,0 +1,10 @@
+def absolute_value(num):
+
+  if num >= 0:
+    return num
+  else:
+    return -num
+
+print("absolute value of 89 :", absolute_value(89))
+
+print("absolute value of -189 :", absolute_value(-189))
